8d0c3bfed35292cec6bcdc957fd0cda70e43770e
Kostian <kostia.talamaniuk@gmail.com>
2025-11-16 17:30:18 +0000

    initial

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => ({
-  base: '/goit-advancedjs-hw-03/',
+  base: '/goit-advancedjs-hw-04/',
   define: { [command === 'serve' ? 'global' : '_global']: {} },
   root: 'src',
   server: {
